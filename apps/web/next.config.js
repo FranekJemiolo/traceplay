d3940fb34d0acea387aeee281aee41baea5d0072
Franek Jemiolo <f.jemiolo@gmail.com>
2026-04-26 09:58:45 +0200

    Remove transpilePackages to eliminate build-time dependencies

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['@traceplay/ui', '@traceplay/embed-sdk'],
   output: 'export',
   images: {
     unoptimized: true,
