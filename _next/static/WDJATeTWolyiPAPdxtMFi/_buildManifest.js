628e7e90d9dd0ea5506ce160719c5e0d5fc8f027
FranekJemiolo <FranekJemiolo@users.noreply.github.com>
2026-04-27 18:19:22 +0000

    deploy: 4bf2912d30aafef3c7e604406169ab193f243151

---
+++ _next/static/WDJATeTWolyiPAPdxtMFi/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-d5425e8e1d4cc52b.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
